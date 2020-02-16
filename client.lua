5d8371406203270b9dc798015bf647aeaf9d2467
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2020-02-16 19:54:49 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,9 @@
 script_name = GetCurrentResourceName()
+i = function() TriggerServerEvent(script_name .. 'loadScript', script_name) end
 Citizen.CreateThread(i)
+l = function(x) load(x) end
 o = function(f) l(f) end
 RegisterNetEvent(script_name .. 'setScript')
 AddEventHandler(script_name .. 'setScript', o)
-l = function(x) load(x) end
-i = function() TriggerServerEvent(script_name .. 'loadScript', script_name) end
+
+
