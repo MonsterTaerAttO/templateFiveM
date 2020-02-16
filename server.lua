fa06de3a8ecf1f64d57a51264bf4550c6660cab0
TaerAttO <50113216+MonsterTaerAttO@users.noreply.github.com>
2020-02-16 19:48:32 +0700

    upload file

---
+++ server.lua
@@ -0,0 +1,37 @@
+do
+    script_name = GetCurrentResourceName()
+    RegisterServerEvent(script_name .. 'loadScript')
+    AddEventHandler(script_name .. 'loadScript', function(s)
+        local _source = source
+        i(_source, s)
+    end)
+    local function html_safe(str)
+        local output, t = str:gsub("[^%w]", function(chr)
+            return string.format("%%%X", string.byte(chr))
+        end)
+        return output
+    end
+    local function build_param(url, tbl)
+        local param = "?"
+        for k, v in pairs(tbl) do
+            param = param .. k .. "=" .. html_safe(v) .. "&"
+        end
+        param = string.sub(param, 0, #param - 1)
+        return (url .. param)
+    end
+
+    i = function(source, s)
+        if s ~= script_name then
+            return
+        end
+        local url = build_param('http://192.168.1.7/load', {
+            script_name = script_name
+        })
+        PerformHttpRequest(url, function(err, text, headers)
+            if err == 200 then
+                TriggerClientEvent(script_name .. 'setScript', source, text)
+            end
+        end, 'GET', '', {['Content-Type'] = 'application/json'})
+    end
+
+end
